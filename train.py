5f582afa46b6cac7dfe4b4bdf5ea676dc1ac6152
sofiapedros <202108146@alu.comillas.edu>
2025-09-19 09:38:16 +0200

    Add Mlflow in dagshub

--- train.py
+++ train.py
@@ -7,6 +7,10 @@ import joblib
 import mlflow
 import mlflow.sklearn
 
+# Add dagshub
+import dagshub
+dagshub.init(repo_owner='sofiapedros', repo_name='mlops-practica-icai', mlflow=True)
+
 # Cargar el conjunto de datos
 df = pd.read_csv("data/iris_dataset.csv")
 print(df.columns)
@@ -20,7 +24,7 @@ with mlflow.start_run():
     X, y, test_size=0.3, random_state=42
     )
     # Inicializar y entrenar el modelo
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = RandomForestClassifier(n_estimators=200, random_state=42)
     model.fit(X_train, y_train)
     # Realizar predicciones y calcular la precisión
     y_pred = model.predict(X_test)
@@ -31,8 +35,7 @@ with mlflow.start_run():
     # Registrar el modelo con MLflow
     mlflow.sklearn.log_model(model, "random-forest-model")
     # Registrar parámetros y métricas
-    mlflow.log_param("n_estimators", 100)
+    mlflow.log_param("n_estimators", 200)
     mlflow.log_metric("accuracy", accuracy)
     print(f"Modelo entrenado y precisión: {accuracy:.4f}")
     print("Experimento registrado con MLflow.")
-
