0e54aa9212035c65ee277f6609bf6cf76a17b690
sofiapedros <202108146@alu.comillas.edu>
2025-09-26 08:14:27 +0200

    Update train

--- train.py
+++ train.py
@@ -1,21 +1,28 @@
+import os
 import pandas as pd
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 import joblib
 import mlflow
 import mlflow.sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Add dagshub
 import dagshub
 dagshub.init(repo_owner='sofiapedros', repo_name='mlops-practica-icai', mlflow=True)
 
-# Cargar el conjunto de datos
-df = pd.read_csv("data/iris_dataset.csv")
-print(df.columns)
-X = df[[" feature_1", " feature_2"]].values
-y = df["data_version_1"].values
+# Cargar el conjunto de datos desde el archivo CSV
+try:
+ iris = pd.read_csv('data/iris_dataset.csv')
+except FileNotFoundError:
+ print("Error: El archivo 'data/iris_dataset.csv' no fue encontrado.")
+
+# Dividir el DataFrame en características (X) y etiquetas (y)
+X = iris.drop('target', axis=1)
+y = iris['target']
 
 
 with mlflow.start_run():
@@ -24,7 +31,7 @@ with mlflow.start_run():
     X, y, test_size=0.3, random_state=42
     )
     # Inicializar y entrenar el modelo
-    model = RandomForestClassifier(n_estimators=200, random_state=42)
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
     # Realizar predicciones y calcular la precisión
     y_pred = model.predict(X_test)
@@ -35,7 +42,7 @@ with mlflow.start_run():
     # Registrar el modelo con MLflow
     mlflow.sklearn.log_model(model, "random-forest-model")
     # Registrar parámetros y métricas
-    mlflow.log_param("n_estimators", 200)
+    mlflow.log_param("n_estimators", 100)
     mlflow.log_metric("accuracy", accuracy)
     print(f"Modelo entrenado y precisión: {accuracy:.4f}")
     print("Experimento registrado con MLflow.")
